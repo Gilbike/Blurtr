03e02b9d2b729056fcab5e08d78625b98f92a489
Gilbike <marton.dikacz@gmail.com>
2024-02-29 10:38:52 +0100

    feat: :lipstick: add blurt question component

---
+++ src/lib/components/BlurtField.svelte
@@ -0,0 +1,29 @@
+<form class="flex flex-col gap-2">
+  <div>
+    <label for="question">Kérdés</label>
+    <input type="text" name="question" id="question" />
+  </div>
+
+  <div>
+    <label for="anwser">Válasz</label>
+    <textarea name="anwser" id="anwser" rows="6" />
+  </div>
+</form>
+
+<style scoped>
+  form * {
+    @apply block w-full;
+  }
+
+  label {
+    @apply text-neutral-800 font-light;
+  }
+
+  input, textarea {
+    @apply border-2 border-black px-2 py-1 outline-none;
+  }
+
+  textarea {
+    @apply resize-none
+  }
+</style>
