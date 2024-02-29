89cb30df2e6f818f747a37f288a0518bd6c5824b
Gilbike <marton.dikacz@gmail.com>
2024-02-29 10:45:27 +0100

    feat: :lipstick: creation page frame complete

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,3 +1,3 @@
-<div class="m-auto w-11/12 md:w-9/12 lg:w-7/12 xl:w-5/12">
+<div class="m-auto w-11/12 md:w-9/12 lg:w-7/12 xl:w-5/12 flex flex-col h-[100vh]">
   <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import BlurtField from "$lib/components/BlurtField.svelte";
+  import Container from "$lib/components/Container.svelte";
+
+</script>
+
+<Container>
+  <h1 class="mt-3 font-bold text-2xl">Blurt létrehozása</h1>
+
+  <div class="overflow-y-auto h-full py-3">
+    <div class="flex flex-col gap-2 divide-y-[1px] divide-neutral-700/25">
+
+    </div>
+    <button class="bg-blue-400 mt-3 w-full py-1 text-white hover:bg-blue-500 transition">
+      Új kérdés
+    </button>
+  </div>
+</Container>
